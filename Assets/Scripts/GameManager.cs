f349635f91afd0a36778d8ae5c031361790fbfc5
ColdFox <gta8mil@gmail.com>
2026-01-17 20:57:16 -0300

    GameManager UpdateScore Function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,30 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject ballPrefab;
+    int scoreP1, scoreP2;
+    [SerializeField] TMP_Text txtScoreP1, txtScoreP2;
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        scoreP1 = scoreP2 = 0;
     }
 
     public void Goal(GameObject activeBall, int directionX)
     {
         Destroy(activeBall);
 
+        UpdateScore((directionX > 0) ? 1 : 2);
+
         GameObject newBall = Instantiate(ballPrefab, Vector3.zero, Quaternion.identity);
         // Player who conceded the goal starts next.
         newBall.GetComponent<BallMovement>().directionX = directionX;
+    }
+
+    void UpdateScore(int player)
+    {
+        if(player == 1) txtScoreP1.text = (++scoreP1).ToString();
+        else txtScoreP2.text = (++scoreP2).ToString();
     }
 }
