42d4375c451eb27bb0c4747d4a87fa50b4ff1644
ColdFox <gta8mil@gmail.com>
2026-01-29 20:50:51 -0300

    Unity 6.3 LTS and Movement Physics on FixedUpdate

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField, Range(1, 2)] int player;
     [SerializeField] float moveSpeed;
     InputAction moveAction;
-
+    Vector2 moveValue;
+    Rigidbody2D rb;
+    
     void OnEnable() => GameEvents.OnGameEnded += DisableMovement;
     void OnDisable() => GameEvents.OnGameEnded -= DisableMovement;
 
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
         moveAction = InputSystem.actions.FindActionMap("Player" + player).FindAction("Movement");
         moveAction.Enable();
     }
 
-    void Update()
-    {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        if(moveValue != Vector2.zero) transform.Translate(moveSpeed * Time.deltaTime * new Vector3(0, moveValue.y));
-    }
-
+    void Update() => moveValue = moveAction.ReadValue<Vector2>();
+    void FixedUpdate() => rb.linearVelocity = moveValue * moveSpeed;
     void DisableMovement(int _) => moveAction.Disable();
 }
